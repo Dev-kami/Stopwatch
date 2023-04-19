43349130a9303a3ec9b4161adfda5050e9d10e7b
Dev-kami <blossomobasi2@gmail.com>
2023-04-19 13:17:27 +0100

    stop watch

---
+++ script.js
@@ -0,0 +1,46 @@
+// Selectors
+const stopBtn = document.querySelector(".stop-btn");
+const playBtn = document.querySelector(".play-btn");
+const resetBtn = document.querySelector(".reset-btn");
+const displayTime = document.querySelector(".display-time");
+
+// Adding Events
+playBtn.addEventListener("click", watchStart);
+stopBtn.addEventListener("click", watchStop);
+resetBtn.addEventListener("click", watchReset);
+
+let [seconds, minutes, hours] = [0, 0, 0];
+let timer = null;
+
+function stopwatch() {
+  seconds++;
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes === 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+
+  displayTime.innerHTML = `${h}:${m}:${s}`;
+}
+
+function watchStart() {
+  if (timer !== null) {
+    clearInterval(timer);
+  }
+  timer = setInterval(stopwatch, 1000);
+}
+
+function watchStop() {
+  clearInterval(timer);
+}
+function watchReset() {
+  clearInterval(timer);
+  [seconds, minutes, hours] = [0, 0, 0];
+  displayTime.innerHTML = "00:00:00";
+}
